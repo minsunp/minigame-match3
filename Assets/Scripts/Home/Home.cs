e0a58db4041676a8d9db2137a48d361bddb88483
SChandra96 <shikhaac@andrew.cmu.edu>
2018-04-12 20:17:59 -0400

    Added the minigame project

---
+++ Assets/Scripts/Home/Home.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Home : MonoBehaviour
+{
+    void Start()
+    {
+        // hidden banner (banner only show on Game Play scene)
+        MusicController.Music.BG_menu();
+    }
+
+    void Update()
+    {
+        // Exit game if click Escape key or back on mobile
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            ExitOK();
+        }
+    }
+
+    /// <summary>
+    /// Exit game
+    /// </summary>
+    public void ExitOK()
+    {
+        Application.Quit();
+    }
+
+}
